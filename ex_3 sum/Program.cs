b14fba58c7f73168634bcde080d9549554df9bdc
tolseone <asemisynov@mail.ru>
2023-02-18 18:58:13 +0300

    add changes to ex_3

--- ex_3 sum/Program.cs
+++ ex_3 sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int number_1 = 3;
-int number_2 = 4;
+﻿int number_1 = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(number_1);
+int number_2 = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(number_2);
 int result = number_1 + number_2;
 Console.WriteLine(result);
