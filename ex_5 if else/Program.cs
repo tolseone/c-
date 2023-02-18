422ac468ab36727f33d9913eb467de4aff6e9d15
tolseone <asemisynov@mail.ru>
2023-02-18 19:12:21 +0300

    add ex_5 if else

---
+++ ex_5 if else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "толик")
+{
+    Console.WriteLine("Ура, это же Еболик первый день!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
