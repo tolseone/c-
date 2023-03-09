16f986c7a492eacd0cf083995102d7a058aaa00a
tolseone <asemisynov@mail.ru>
2023-03-09 17:22:38 +0300

    исправил 4 упражнение

--- ex_3 sum/Program.cs
+++ ex_3 sum/Program.cs
@@ -2,5 +2,4 @@
 Console.WriteLine(number_1);
 int number_2 = new Random().Next(1,10); // 1 2 3 4 ... 9
 Console.WriteLine(number_2);
-int result = number_1 + number_2;
-Console.WriteLine(result);
+Console.WriteLine(number_1 + number_2);
--- ex_4 div/Program.cs
+++ ex_4 div/Program.cs
@@ -1,3 +1,3 @@
-﻿double number_1 = 12;
-double number_2 = 5;
+﻿double number_1 = new Random().Next(1,100);
+double number_2 = new Random().Next(1,100);
 Console.WriteLine(number_1 / number_2);
