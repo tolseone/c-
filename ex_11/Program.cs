bbde1909181d37b781938cbb642c7831c37c86fc
tolseone <asemisynov@mail.ru>
2023-03-10 14:55:24 +0300

    Цикл for

---
+++ ex_11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Метод 4 через цикл for
+string Method4(int count, string c)
+{
+    string result = String.Empty;
+
+    for ( int i = 0; i < count; i++)
+    {
+        result += c;
+    }
+    return result;
+}
+
+string res = Method4(10, "Z");
+Console.WriteLine(res);
