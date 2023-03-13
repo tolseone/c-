85483ce3cd7a8300384023ba265ddf3502edba1f
tolseone <asemisynov@mail.ru>
2023-03-13 13:20:56 +0300

    рекурсия метод факториала

---
+++ 4 lection_ex_03_factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿double Factorial(int n)
+{
+    // 1! = 1;
+    // 0! = 1;
+    if( n == 1 || n == 0) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 0; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
